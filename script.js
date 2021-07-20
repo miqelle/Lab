b7efafe178716808139dc1aab1d5a9e572535cbb
miqelle <miqellec3@gmail.com>
2021-07-20 18:36:26 -0500

    lab almost complete

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+let userName = prompt("What's your name?");
+console.log(userName);
+alert("Welcome to my page " + userName + "!");
+
+let learningQuestion = prompt("Are you learning JavaScript?");
+console.log(learningQuestion);
+alert(" I'm glad you're learning " + userName + " !");
+
+let knowLang = prompt("Do you know HTML & CSS?");
+console.log(knowLang);
+alert(userName + " those are great languages to know!")
+
+let likeQuestion = prompt("Do you like to code?");
+console.log(likeQuestion);
+alert("That's awesome, coding is super cool " + userName + "!");
+let visQuestion = prompt("Do you like Visual Studio Code?");
+console.log(visQuestion);
+alert(userName + " Visual Studio Code is the best IDE!");
+
+

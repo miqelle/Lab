f3d2345a36376463f1ed61c9b1ac03ea2dc1d25c
miqelle <miqellec3@gmail.com>
2021-07-20 21:07:15 -0500

    added strict equals

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 'use strict';
-let userName = prompt("What's your name?");
+let userName = prompt("Hi, what's your name?");
 console.log(userName);
 alert("Welcome to my page " + userName + "!");
-
-let learningQuestion = prompt("Are you learning JavaScript?");
-console.log(learningQuestion);
-alert(" I'm glad you're learning!");
+alert(userName + " I'm going to ask you a series of questions!");
+alert("You can only answer yes or no!");
+let addQuestion = prompt("Does 1 + 1 = 2 ?");
+if ( addQuestion === 'yes') {
+console.log("Good job, that's correct");
+}
+if ( addQuestion === 'no') {
+    console.log("Try again")
+}
+alert("Basic Math is essential knowledge!");
 
 let knowLang = prompt("Do you know HTML & CSS?");
-console.log(knowLang);
-alert("Those are great languages to know!")
+if ( knowLang === 'yes') {
+console.log("Great job, keep learning!");
+}
+if ( knowLang === 'no') {
+    console.log("Wrong answer, you should study!");
+}
+alert("Those are great languages to know!");
 
 let likeQuestion = prompt("Do you like to code?");
-console.log(likeQuestion);
+if (likeQuestion === 'yes') {
+    console.log("You'll be a pro in no time!");
+}
+if ( likeQuestion === 'no') {
+    console.log("Maybe one day you will like coding!");
+}
 alert("That's awesome, coding is super cool!");
+
 let visQuestion = prompt("Do you like Visual Studio Code?");
-console.log(visQuestion);
-alert(userName + "," + " that's great cause Visual Studio Code is the best IDE!");
+if ( visQuestion === 'yes'); {
+    console.log("You have exquisite taste!");
+}
+if ( visQuestion === 'no'); {
+    console.log("You have poor taste in IDE's");
+}
+alert("Welcome " + userName + " let's explore the site!");
 
 

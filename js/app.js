406e46bb6ef6336adbda8f51c3c549a56041866b
miqelle <miqellec3@gmail.com>
2021-07-21 13:09:37 -0500

    added more to script

--- js/app.js
+++ js/app.js
@@ -1,43 +1,52 @@
 'use strict';
 let userName = prompt("Hi, what's your name?");
-console.log(userName);
 alert("Welcome to my page " + userName + "!");
 alert(userName + " I'm going to ask you a series of questions!");
 alert("You can only answer yes or no!");
+
 let addQuestion = prompt("Does 1 + 1 = 2 ?");
-if ( addQuestion === 'yes') {
-console.log("Good job, that's correct");
+let answer = ("yes");
+
+if ( addQuestion === answer ) 
+{
+    alert("Good job that's correct 😊");
 }
-if ( addQuestion === 'no') {
-    console.log("Try again")
+else ( addQuestion === 'no');
+{
+    alert("Try again");
 }
-alert("Basic Math is essential knowledge!");
 
 let knowLang = prompt("Do you know HTML & CSS?");
-if ( knowLang === 'yes') {
-console.log("Great job, keep learning!");
+let knowAnswer = "yes";
+if ( knowLang === knowAnswer)
+{
+    alert("Great job, keep learning!");
 }
-if ( knowLang === 'no') {
-    console.log("Wrong answer, you should study!");
+else ( knowLang === 'no')
+{
+    alert("Wrong answer, you should study!");
 }
-alert("Those are great languages to know!");
 
 let likeQuestion = prompt("Do you like to code?");
-if (likeQuestion === 'yes') {
-    console.log("You'll be a pro in no time!");
+let likeAnswer = "yes";
+if (likeQuestion === likeAnswer) 
+{
+    alert("You'll be a pro in no time!");
 }
-if ( likeQuestion === 'no') {
-    console.log("Maybe one day you will like coding!");
+else( likeQuestion === 'no') 
+{
+    alert("Maybe one day you will like coding!");
 }
-alert("That's awesome, coding is super cool!");
 
 let visQuestion = prompt("Do you like Visual Studio Code?");
-if ( visQuestion === 'yes'); {
-    console.log("You have exquisite taste!");
+let visAnswer = "yes";
+if ( visQuestion === visAnswer); 
+{
+    alert("You have exquisite taste!");
 }
-if ( visQuestion === 'no'); {
-    console.log("You have poor taste in IDE's");
+elseif( visQuestion === "no"); 
+{
+    alert("You have poor taste in IDE's");
+    prompt("Feel free to explore the page " + userName );
 }
-alert("Welcome " + userName + " let's explore the site!");
-
 

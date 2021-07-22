ed5ed007acd21753f5b5da25b3f122e6ef6d49d2
miqelle <miqellec3@gmail.com>
2021-07-21 21:57:57 -0500

    updated html with top 10, and updated script conditionals

--- js/app.js
+++ js/app.js
@@ -7,46 +7,46 @@ alert("You can only answer yes or no!");
 let addQuestion = prompt("Does 1 + 1 = 2 ?");
 let answer = ("yes");
 
-if ( addQuestion === answer ) 
+if ( addQuestion === 'y' || addQuestion === 'yes' ) 
 {
     alert("Good job that's correct 😊");
 }
-else ( addQuestion === 'no');
+else
 {
     alert("Try again");
 }
 
 let knowLang = prompt("Do you know HTML & CSS?");
 let knowAnswer = "yes";
-if ( knowLang === knowAnswer)
+if ( knowLang === 'y' || knowLang ==='yes')
 {
     alert("Great job, keep learning!");
 }
-else ( knowLang === 'no')
+else
 {
     alert("Wrong answer, you should study!");
 }
 
 let likeQuestion = prompt("Do you like to code?");
 let likeAnswer = "yes";
-if (likeQuestion === likeAnswer) 
+if (likeQuestion === 'y' || likeQuestion === 'yes') 
 {
     alert("You'll be a pro in no time!");
 }
-else( likeQuestion === 'no') 
+else
 {
     alert("Maybe one day you will like coding!");
 }
 
 let visQuestion = prompt("Do you like Visual Studio Code?");
 let visAnswer = "yes";
-if ( visQuestion === visAnswer); 
+if ( visQuestion === 'y' ||  visQuestion === 'yes')
 {
     alert("You have exquisite taste!");
 }
-elseif( visQuestion === "no"); 
+else
 {
-    alert("You have poor taste in IDE's");
-    prompt("Feel free to explore the page " + userName );
+    alert("Visual Studio Code is a good choice!");
 }
+alert("Feel free to explore the page " + userName + " 😊" + "!");
 
